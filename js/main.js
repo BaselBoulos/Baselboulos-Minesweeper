6bfff74acc462550ac4c898f398b88664491a52e
BaselBoulos <baselboulos@gmail.com>
2021-10-04 20:22:01 +0300

    Backup update and changes - First delivery

--- js/main.js
+++ js/main.js
@@ -4,24 +4,23 @@ const MINE = '💣'
 const FLAG = '🚩'
 const EMPTY = '🗅'
 
-var gGame
-var gLevel
 var gBoard
-var isTimerOn = false
+var gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 }
+var gLevel = { SIZE: 4, MINES: 2 }
+
+var gFlaggedMinesCount
+var isTimerOn
 var gTimerInterval
-var gFlaggedMinesCount = 0
+var gMinesPos = []
 
-gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 }
-gLevel = { SIZE: 4, MINES: 2 }
-
-// This is called when page loads
 function initGame() {
   gBoard = buildBoard(gLevel.SIZE, gLevel.MINES)
-  gGame.isOn = true
+  gFlaggedMinesCount = 0
   printMat(gBoard, '.board-container')
+  isTimerOn = false
+  gGame.isOn = true
 }
 
-// Builds the board Set mines at random locations Call setMinesNegsCount() Return the created board
 function buildBoard(size) {
   var board = []
   for (var i = 0; i < size; i++) {
@@ -35,45 +34,39 @@ function buildBoard(size) {
       }
     }
   }
-  board = setMinesOnBoard(board)
+  board = setMinesOnBoard(board, gLevel.MINES)
   setMinesNegsCount(board)
   printBoard(board)
   return board
 }
 
-// Count mines around each cell and set the cell's minesAroundCount.
-function setMinesOnBoard(board) {
-  for (var i = 0; i < gLevel.MINES; i++) {
-    var pos = getRandPos(board)
-    board[pos.i][pos.j].isMine = true
+function setMinesOnBoard(board, minesCount) {
+  for (var i = 0; i < minesCount; i++) {
+    var pos = getRandNonMinePos(board)
+    gMinesPos.push(pos)
+    board[pos.i][pos.j].isMine = !board[pos.i][pos.j].isMine
   }
   return board
 }
 
-function getRandPos(board) {
-  var nonMineCells = getNonMinesCells(board)
-  if (!nonMineCells.length) return
-  var randIdx = getRandomInt(0, nonMineCells.length - 1)
-  var randPos = nonMineCells[randIdx]
-  return randPos
-}
-
-function getNonMinesCells(board) {
+function getRandNonMinePos(board) {
   var nonMineCells = []
   for (var i = 0; i < board.length; i++) {
     for (var j = 0; j < board[0].length; j++) {
-      if (!board[i][j].isMine) {
-        nonMineCells.push({ i: i, j: j })
-      }
+      if (!board[i][j].isMine) nonMineCells.push({ i: i, j: j })
     }
   }
-  return nonMineCells
+  if (!nonMineCells.length) return
+  var randIdx = getRandomInt(0, nonMineCells.length - 1)
+  var randCellPos = nonMineCells[randIdx]
+  return randCellPos
 }
 
 function setMinesNegsCount(board) {
+  var mineNegsCount = 0
   for (var i = 0; i < board.length; i++) {
     for (var j = 0; j < board.length; j++) {
-      var mineNegsCount = countMineNegs(i, j, board)
+      mineNegsCount = countMineNegs(i, j, board)
       board[i][j].minesAroundCount = mineNegsCount
     }
   }
@@ -106,7 +99,8 @@ function cellClicked(elCell, i, j, event) {
 
   // Right click stuff
   if (event.button === 2) {
-    cellMarked(elCell, i, j)
+    cellMarked(i, j)
+    isVictory()
     return
   }
 
@@ -120,7 +114,10 @@ function cellClicked(elCell, i, j, event) {
     gameOver()
     return
   }
+
   if (!gBoard[i][j].minesAroundCount) {
+    // means no negs around we should open all this elCell negs
+    expandShown(gBoard, i, j)
     gBoard[i][j].isShown = true
     renderCell(pos, EMPTY)
   } else if (gBoard[i][j].minesAroundCount > 0) {
@@ -128,76 +125,7 @@ function cellClicked(elCell, i, j, event) {
     gBoard[i][j].isShown = true
     renderCell(pos, minesAroundCnt)
   }
-
   isVictory()
-}
-
-function resetGame() {
-  var timer = document.querySelector('.timer')
-  timer.innerText = '00:00:00'
-  clearInterval(gTimerInterval)
-  gTimerInterval = null
-  initGame()
-}
-
-/*
-Called on right click to mark a cell that is suspected to be a mine
-*/
-function cellMarked(elCell, i, j) {
-  var pos = { i, j }
-  if (!gBoard[i][j].isMarked) {
-    renderCell(pos, FLAG)
-    gFlaggedMinesCount = gBoard[i][j].isMine
-      ? gFlaggedMinesCount + 1
-      : gFlaggedMinesCount
-  } else {
-    renderCell(pos, '')
-    gFlaggedMinesCount = gBoard[i][j].isMine
-      ? gFlaggedMinesCount - 1
-      : gFlaggedMinesCount
-  }
-  gBoard[i][j].isMarked = !gBoard[i][j].isMarked
-}
-
-function renderCell(pos, value) {
-  var elCell = document.querySelector(`.cell${pos.i}-${pos.j}`)
-  elCell.innerHTML = value
-}
-
-function gameOver() {
-  for (var i = 0; i < gBoard.length; i++) {
-    for (var j = 0; j < gBoard.length; j++) {
-      var cell = gBoard[i][j]
-      var pos = { i, j }
-      if (cell.isMine) renderCell(pos, MINE)
-    }
-    gGame.isOn = false
-    clearInterval(gTimerInterval)
-    gTimerInterval = null
-  }
-}
-
-function isVictory() {
-  var totalCells = gLevel.SIZE ** 2
-  var revealedCellsCnt = 0
-  for (var i = 0; i < gBoard.length; i++) {
-    for (var j = 0; j < gBoard.length; j++) {
-      var cell = gBoard[i][j]
-      if (!cell.isMine && cell.isShown) {
-        revealedCellsCnt++
-      }
-    }
-  }
-  var playedCellsCount = revealedCellsCnt + gFlaggedMinesCount
-  var isWinner = playedCellsCount === totalCells ? true : false
-  if (isWinner) {
-    console.log('You are a Winner')
-    gGame.isOn = false
-    clearInterval(gTimerInterval)
-    gTimerInterval = null
-  } else {
-    console.log('not a winner yet')
-  }
 }
 
 /*
@@ -205,7 +133,75 @@ When user clicks a cell with no mines around, we need to open not only that cell
 NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors 
 BONUS: if you have the time later, try to work more like the real algorithm (see description at the Bonuses section below)
 */
-function expandShown(board, elCell, i, j) {}
+function expandShown(board, cellI, cellJ) {
+  for (var i = cellI - 1; i <= cellI + 1; i++) {
+    if (i < 0 || i >= board.length) continue
+    for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+      if (i === cellI && j === cellJ) continue
+      if (j < 0 || j >= board[i].length) continue
+      var pos = { i, j }
+      if (!gBoard[i][j].isMarked) {
+        board[i][j].isShown = true
+        renderCell(pos, board[i][j].minesAroundCount)
+      }
+      isVictory()
+    }
+  }
+}
+
+// TODO: To refactor this function
+function cellMarked(i, j) {
+  var pos = { i, j }
+  if (!gBoard[i][j].isMarked) renderCell(pos, FLAG)
+  else renderCell(pos, '')
+  gBoard[i][j].isMarked = !gBoard[i][j].isMarked
+  if (gBoard[i][j].isMine && gBoard[i][j].isMarked) gFlaggedMinesCount++
+  else if (gBoard[i][j].isMine && !gBoard[i][j].isMarked) gFlaggedMinesCount--
+  //   console.log(gFlaggedMinesCount)
+}
+
+function resetGame() {
+  var timer = document.querySelector('.timer')
+  timer.innerText = '00:00:00'
+  clearInterval(gTimerInterval)
+  if (gTimerInterval) gTimerInterval = null
+  isTimerOn = !isTimerOn
+  initGame()
+}
+
+function gameOver() {
+  for (var i = 0; i < gMinesPos.length; i++) {
+    var currMinePos = gMinesPos[i]
+    gBoard[currMinePos.i][currMinePos.j].isShown = true
+    renderCell(currMinePos, MINE)
+  }
+  gGame.isOn = false
+  clearInterval(gTimerInterval)
+  if (gTimerInterval) gTimerInterval = null
+}
+
+function isVictory() {
+  var totalCells = gLevel.SIZE ** 2
+  var revealedCount = 0
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard.length; j++) {
+      var cell = gBoard[i][j]
+      if (!cell.isMine && cell.isShown) {
+        revealedCount++
+      }
+    }
+  }
+  var playedCellsCount = revealedCount + gFlaggedMinesCount
+  var isWinner = playedCellsCount === totalCells ? true : false
+  if (isWinner) {
+    console.log('You are a Winner')
+    gGame.isOn = false
+    clearInterval(gTimerInterval)
+    if (gTimerInterval) gTimerInterval = null
+  } else {
+    console.log('not a winner yet')
+  }
+}
 
 function setMode(elMode) {
   switch (elMode.dataset.level) {
@@ -241,6 +237,11 @@ function startTimer(startTime) {
     if (seconds < 10) seconds = '0' + seconds
     elTimer.innerHTML = `${hour}:${minute}:${seconds}`
   }, 1000)
+}
+
+function renderCell(pos, value) {
+  var elCell = document.querySelector(`.cell${pos.i}-${pos.j}`)
+  elCell.innerHTML = value
 }
 
 // Currently only used for debugging
